4998c9a9a1f86e1cd080a3daf462a2a84a3c55eb
MdSaifulIslam <mohammadsaifulcsepstu@gmail.com>
2026-07-24 08:07:22 +0900

    Operator overloading - [ostream, istrem,  int + Integer]

--- Learning_C++/Operator Overloading/main.cpp
+++ Learning_C++/Operator Overloading/main.cpp
@@ -3,21 +3,37 @@
 
 using namespace std;
 
-//Integer operator + (const Integer& a, const Integer& b) {
-//	Integer temp;
-//	temp.setValue(a.getValue() + b.getValue());
-//	return temp;
-//}
+Integer operator + (int a, const Integer& b) {
+	Integer temp;
+	temp.setValue(a + b.getValue());
+	return temp;
+}
+
+ostream& operator  << (ostream& out, const Integer& a) {
+	out << a.getValue() << " ";
+	return out;
+}
+
+istream& operator >> (istream& input, Integer& a) {
+	int x;
+
+	input >> x;
+	a.setValue(x);
+
+	return input;
+}
 
 int main() {
 
 	Integer a(1), b(3);
-	Integer sum = a + b;
-	sum++;
-	++sum;
-	cout << (sum++).getValue() << endl;
-	cout << sum.getValue() << endl;
-	cout << ((a == sum) == 0 ? "Not same" : "Same") << endl;
+	
+	cin >> a;
+
+	Integer sum1 = a + b;
+	Integer sum2 = a + 5;
+	Integer sum3 = 6 + a;
+
+	cout << sum1 << sum2 << sum3 << endl;
 
 	return 0;
 }
