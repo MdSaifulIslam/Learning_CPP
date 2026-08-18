d7acb2c092421c3ac0f4cc7e8504c73c98f657f3
MdSaifulIslam <mohammadsaifulcsepstu@gmail.com>
2026-08-19 02:40:30 +0900

    - user-defined / custom literals

--- Learning_C++/CppUtils/main.cpp
+++ Learning_C++/CppUtils/main.cpp
@@ -19,40 +19,65 @@ void FillColor(Color color) {
 
 enum class TrafficLight :char { RED = 'b', GREEN, YELLOW };
 
+class Distance {
+	long double m_KMs;
+public:
+	Distance(long double km) : m_KMs{ km } {}
+	long double getKm()const { return m_KMs; }
+	void setKM(long double val_KM) { m_KMs = val_KM; }
+};
+
+Distance operator"" _mi(long double val) {
+	return Distance(val * 1.6);
+}
+
+Distance operator"" _metres(long double val) {
+	return Distance(val / 1000);
+}
+
 int main() {
 
-	string s = "hello";
-	s[0] = 'W';
-	char ch = s[2];
+	Distance dist1{ 199.99 };
+	cout << dist1.getKm() << " KM" << endl;
 
-	cout << s << endl;
-	//cin >> s;
-	cout << s.length() << endl;
+	Distance dist2{ 100.0_mi };
+	cout << dist2.getKm() << " KM" << endl;
 
-	string s1{ "hello" }, s2{ "world" };
-	s = s1 + s2;
-	s += s1;
-	cout << s << endl;
+	Distance dist3{ 1999.0_metres };
+	cout << dist3.getKm() << " KM" << endl;
 
-	s.insert(6, "New Insert");
-	cout << s << endl;
+	//string s = "hello";
+	//s[0] = 'W';
+	//char ch = s[2];
 
-	s.erase();
-	s.clear();
+	//cout << s << endl;
+	////cin >> s;
+	//cout << s.length() << endl;
 
-	stringstream ss;
-	int a{ 5 }, b{ 6 };
-	int sum = a + b;
+	//string s1{ "hello" }, s2{ "world" };
+	//s = s1 + s2;
+	//s += s1;
+	//cout << s << endl;
 
-	ss << "Sum of " << a << " & " << b << " is: " << sum << endl;
-	cout << ss.str() << endl;
+	//s.insert(6, "New Insert");
+	//cout << s << endl;
 
-	string intData = "12 15 80, abc, 77";
-	ss.str(intData);
+	//s.erase();
+	//s.clear();
 
-	while (ss >> a) {
-		cout << a << endl; // it will print till 80;
-	}
+	//stringstream ss;
+	//int a{ 5 }, b{ 6 };
+	//int sum = a + b;
+
+	//ss << "Sum of " << a << " & " << b << " is: " << sum << endl;
+	//cout << ss.str() << endl;
+
+	//string intData = "12 15 80, abc, 77";
+	//ss.str(intData);
+
+	//while (ss >> a) {
+	//	cout << a << endl; // it will print till 80;
+	//}
 
 	//Color c = Color::RED;
 	//FillColor(c);
