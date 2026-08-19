e7ee992377479065d5018c58033764daed3c272d
MdSaifulIslam <mohammadsaifulcsepstu@gmail.com>
2026-08-20 06:12:48 +0900

    - multiple catch block

--- Learning_C++/ExceptionHandling/main.cpp
+++ Learning_C++/ExceptionHandling/main.cpp
@@ -4,6 +4,13 @@
 using namespace std;
 
 int processRecords(int count) {
+
+	if (count < 10) {
+		throw out_of_range("Count should be greater than 10");
+	}
+	
+	int* p = new int[count]; // through bad_alloc exception
+
 	int* pArr = (int*)malloc(count * sizeof(int));
 	if (pArr == nullptr) {
 		throw runtime_error("Failed to allocate Memory.");
@@ -20,9 +27,22 @@ int main() {
 
 	try {
 		processRecords(numeric_limits<int>::max());
+		//processRecords(5);
 	}
 	catch(runtime_error &ex){
 		cout << ex.what() << endl;
+	}
+	catch (out_of_range &ex) {
+		cout << ex.what() << endl;
+	}
+	catch (bad_alloc &ex) {
+		cout << ex.what() << endl;
+	}
+	catch (exception &ex) {
+		cout << ex.what() << endl;
+	}
+	catch (...) {
+		cout << "Exception" << endl;
 	}
 
 
